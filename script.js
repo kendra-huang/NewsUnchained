02a8c265b5f5e1bb98200439c88deb97dfa01a7a
Nick Baker <ndbaker1@outlook.com>
2019-11-09 14:39:00 -0600

    extension base update

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 /**
  * Javascript ~~~
+ * 
+ * 
+ * var url;
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
+        url = tabs[0].url;
+    });
+ * 
  */
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+  });
+
+ this.document.getElementById("new-tab-button").addEventListener("click", function(){ chrome.tabs.create({"url": "http://google.com"}); })
