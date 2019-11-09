0e26ec1376645dbcdb5c0074e1e49385cff3fd44
Nick Baker <ndbaker1@outlook.com>
2019-11-09 16:42:53 -0600

    js and html update

--- script.js
+++ script.js
@@ -6,10 +6,15 @@
     
  * 
  */
+var url;
 
 this.document.getElementById("display-url").addEventListener("click", function(){ 
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
         document.getElementById("text").innerHTML = tabs[0].url;
+        url = tabs[0].url;
     });
 });
 
+function parseText(text){
+
+};
