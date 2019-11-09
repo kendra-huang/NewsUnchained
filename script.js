25ad4414e8b636ff99b16fb0c2438b5f3409f32b
Nick Baker <ndbaker1@outlook.com>
2019-11-09 14:50:59 -0600

    view url debug

--- script.js
+++ script.js
@@ -3,18 +3,13 @@
  * 
  * 
  * var url;
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
-        url = tabs[0].url;
-    });
+    
  * 
  */
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // Send a message to the active tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+this.document.getElementById("display-url").addEventListener("click", function(){ 
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
+        document.getElementById("text").innerHTML = tabs[0].url;
     });
-  });
+});
 
- this.document.getElementById("new-tab-button").addEventListener("click", function(){ chrome.tabs.create({"url": "http://google.com"}); })
