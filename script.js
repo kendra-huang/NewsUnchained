d0f8cca5dc9d93427cc0d3bdb148621291e6e4f8
Nick Baker <ndbaker1@outlook.com>
2019-11-09 19:25:57 -0600

    extension DOM access added

--- script.js
+++ script.js
@@ -3,15 +3,29 @@
  * 
  * 
  * var url;
-    
- * 
- */
-var url;
-
-this.document.getElementById("display-url").addEventListener("click", function(){ 
+    document.getElementById("display-url").addEventListener("click", function(){ 
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
         document.getElementById("text").innerHTML = tabs[0].url;
         url = tabs[0].url;
+    });
+});
+
+ * 
+ */
+
+document.getElementById("display-url").addEventListener('click', () => {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
+        document.getElementById("text").innerHTML = tabs[0].url;
+    });
+    console.log("Popup DOM fully loaded and parsed");
+
+    function modifyDOM() {
+        return document.body.innerHTML;
+    }
+    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+    chrome.tabs.executeScript({ code: '(' + modifyDOM + ')();' }, (results) => {
+        //innerHTML of body logged
+        console.log(results[0]);
     });
 });
 
