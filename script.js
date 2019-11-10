38e3e5872e90355596d602da744a5205b1b83b6f
Kendra Huang <54409323+kendra-huang@users.noreply.github.com>
2019-11-09 21:18:10 -0600

    formatting test

--- script.js
+++ script.js
@@ -13,6 +13,17 @@
  * 
  */
 
+/**
+ * Javascript ~~~
+ * 
+ * 
+ * var url;
+    
+ * 
+ */
+
+ var str = "";
+
 document.getElementById("display-url").addEventListener('click', () => {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
         document.getElementById("text").innerHTML = tabs[0].url;
@@ -25,10 +36,25 @@ document.getElementById("display-url").addEventListener('click', () => {
     //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
     chrome.tabs.executeScript({ code: '(' + modifyDOM + ')();' }, (results) => {
         //innerHTML of body logged
-        console.log(results[0]);
+        // console.log stored as variable
+        str = results[0];
+        console.log(str);
     });
 });
 
-function parseText(text){
+function parseText(str){
+    
+    var arr = ["hello", "hi", "banana"];
+    var arrVal = "";
+    var str = "";
+    for (var i = 0; arr.length(); i++)
+    {
+        arrVal = arr[i];
+        text = document.write("<p>Strike: " + txt.strike() + "</p>");
+        str.replace(str, text);
+    }
+    
+    // arrayBiased = xmlDoc.getElementsByTagName('sentence')[0].childNodes[0].nodeValue;
+
 
 };
